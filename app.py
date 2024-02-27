f6491a6e6a9310608616fe6c897ad0aad6551de2
unknown <chenyih0111@gmail.com>
2024-02-28 01:53:05 +1000

    finish

--- app.py
+++ app.py
@@ -12,9 +12,11 @@ driver.maximize_window()
 workbook = Workbook()
 sheet = workbook.active
 
+page_number = 0
 url = "https://visitors.lineapelle-fair.it/#/event/lineapelle/catalogue/"
 driver.get(url)
-sleep(8)
+sleep(5)
+
 
 def find_element(driver, whichBy, unique: str) -> WebElement:
     while True:
@@ -26,6 +28,7 @@ def find_element(driver, whichBy, unique: str) -> WebElement:
         sleep(3)
     return element
 
+
 def find_elements(driver, whichBy, unique: str) -> list[WebElement]:
     while True:
         try:
@@ -35,6 +38,7 @@ def find_elements(driver, whichBy, unique: str) -> list[WebElement]:
             pass
         sleep(0.1)
     return elements
+
 
 def products_items():
     element = ""
@@ -51,13 +55,11 @@ def products_items():
                 element2 = find_elements(products_category[i], By.TAG_NAME, 'td')[
                     1].find_element(By.TAG_NAME, 'label').text
                 element += f"{element1}:{element2},"
-                print(f"this is items of products===>{element}")
             text = driver.find_element(
                 By.XPATH, '/html/body/div[1]/div[1]/div/div/div/div[1]/div[2]/portlet/div/div[2]/div/div/div[1]/datatable-v2/div/div[2]/div[2]/div[1]/div[1]').text
             if text != True:
                 break
             numbers = re.findall(r'\d+', text)
-            print(f"nubers is ===>{numbers}")
             if int(numbers[1]) == int(numbers[2]):
                 break
             driver.find_element(
@@ -66,56 +68,65 @@ def products_items():
         else:
             break
     return element
-    
+
+
 def get_info():
     try:
         driver.find_element(By.CLASS_NAME, 'm-card-profile__name')
         name = driver.find_element(By.CLASS_NAME, 'm-card-profile__name').text
     except:
         name = ""
-    
+
     try:
         driver.find_element(
             By.CLASS_NAME, 'm-card-profile__email')
         address = driver.find_element(
-        By.CLASS_NAME, 'm-card-profile__email').text
+            By.CLASS_NAME, 'm-card-profile__email').text
     except:
         address = ""
     try:
         driver.find_element(
             By.XPATH, '/html/body/div[1]/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/exhibitor-details/div[1]/div[2]/div[3]/div[2]/a')
-        website = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/exhibitor-details/div[1]/div[2]/div[3]/div[2]/a').text
+        website = driver.find_element(
+            By.XPATH, '/html/body/div[1]/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/exhibitor-details/div[1]/div[2]/div[3]/div[2]/a').text
     except:
         website = ""
     try:
         driver.find_element(
             By.XPATH, '/html/body/div[1]/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/exhibitor-details/div[1]/div[3]/div[2]/div[2]')
         description = driver.find_element(
-        By.XPATH, '/html/body/div[1]/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/exhibitor-details/div[1]/div[3]/div[2]/div[2]').text
+            By.XPATH, '/html/body/div[1]/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/exhibitor-details/div[1]/div[3]/div[2]/div[2]').text
     except:
         description = ""
     try:
-        print("producs is exist")
         driver.find_element(
             By.XPATH, '/html/body/div[1]/div[1]/div/div/div/div[1]/div[2]/portlet/div/div[2]/div/div/div[1]/datatable-v2/div/div[2]/div[1]/div/div/div[1]/div/table/thead/tr/th[1]')
         products = products_items()
     except:
-        print("products is not exist")
         products = ""
+    value = [name, address, website, description, products]
+    sheet.append(value)
+    workbook.save("output.xlsx")
     print(
         f"this is ===>{name}=======>{address}=======>{website}=====>{description}====>{products}")
+
+
 while True:
     try:
         catalogues = find_element(
             driver, By.XPATH, '/html/body/div[1]/div[1]/div/div/div/portlet[2]/div/div[2]/div/div[2]/div/datatable-visitors/div/div[2]/div[1]/div/div[1]/div[2]/table/tbody').find_elements(By.TAG_NAME, 'tr')
         len_catalogues = len(catalogues)
-        page_number = 0
         for i in range(len_catalogues):
-            for page in range(page_number):
-                print("click")
+            if page_number > 0:
+                for j in range(page_number):
+                    prenext = driver.find_element(
+                        By.XPATH, "/ html/body/div[1]/div[1]/div/div/div/portlet[2]/div/div[2]/div/div[2]/div/datatable-visitors/div/div[2]/div[2]/div[2]/div/ul/li[9]")
+                    driver.execute_script("arguments[0].click();", prenext)
+                    sleep(1)
             catalogues = find_element(
                 driver, By.XPATH, '/html/body/div[1]/div[1]/div/div/div/portlet[2]/div/div[2]/div/div[2]/div/datatable-visitors/div/div[2]/div[1]/div/div[1]/div[2]/table/tbody').find_elements(By.TAG_NAME, 'tr')
-            element = find_elements(catalogues[i], By.TAG_NAME, 'td')[0].find_element(By.TAG_NAME, 'label')
+            element = find_elements(catalogues[i], By.TAG_NAME, 'td')[
+                0].find_element(By.TAG_NAME, 'label')
             driver.execute_script("arguments[0].click();", element)
             sleep(5)
 
@@ -126,12 +137,13 @@ while True:
         text = driver.find_element(
             By.XPATH, '/html/body/div[1]/div[1]/div/div/div/portlet[2]/div/div[2]/div/div[2]/div/datatable-visitors/div/div[2]/div[2]/div[1]/div[1]').text
         numbers = re.findall(r'\d+', text)
-        print(f"total nubers is ===>{numbers}")
         if int(numbers[1]) == int(numbers[2]):
+
             break
-        driver.find_element(
-            By.XPATH, "/ html/body/div[1]/div[1]/div/div/div/portlet[2]/div/div[2]/div/div[2]/div/datatable-visitors/div/div[2]/div[2]/div[2]/div/ul/li[9]").click()
+        next = driver.find_element(
+            By.XPATH, "/ html/body/div[1]/div[1]/div/div/div/portlet[2]/div/div[2]/div/div[2]/div/datatable-visitors/div/div[2]/div[2]/div[2]/div/ul/li[9]")
+        driver.execute_script("arguments[0].click();", next)
+        page_number += 1
         sleep(1)
     except Exception as e:
         pass
-
